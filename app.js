37e038100c8ceb76f5642afc8f826a45282b24c1
Ridge <rto@MBAir.local>
2017-06-27 12:31:16 -0700

    add messages to rootnavigator

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ import { StackNavigator } from 'react-navigation';
 import SignIn from './components/SignIn.js';
 import SignUp from './components/SignUp.js';
 import Rooms from './components/Rooms.js';
+import Messages from './components/Messages.js';
 
 const RootNavigator = StackNavigator(
   {
     SignIn: { name: 'SignIn', screen: SignIn },
     SignUp: { name: 'SignUp', screen: SignUp },
-    Rooms: { name: 'Rooms', screen: Rooms }
+    Rooms: { name: 'Rooms', screen: Rooms },
+    Messages: { name: 'Messages', screen: Messages}
   },
   { headerMode: 'screen' }
 );
