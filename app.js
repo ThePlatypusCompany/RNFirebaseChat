91f4b5fbec8701d9d075fff418fa81d02d27e893
Ridge <rto@MBAir.local>
2017-06-27 12:17:49 -0700

    add rooms to rootnavigator

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ import {
 import { StackNavigator } from 'react-navigation';
 import SignIn from './components/SignIn.js';
 import SignUp from './components/SignUp.js';
+import Rooms from './components/Rooms.js';
 
 const RootNavigator = StackNavigator(
   {
     SignIn: { name: 'SignIn', screen: SignIn },
-    SignUp: { name: 'SignUp', screen: SignUp }
+    SignUp: { name: 'SignUp', screen: SignUp },
+    Rooms: { name: 'Rooms', screen: Rooms }
   },
   { headerMode: 'screen' }
 );
