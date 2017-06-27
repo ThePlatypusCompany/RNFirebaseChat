6cf8b18cf3c5071e74ff0f4469ebd7b7fc867ead
Ridge <rto@MBAir.local>
2017-06-27 12:12:16 -0700

    add sign in to rootnavigator

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import {
   AppRegistry
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import SignIn from './components/SignIn.js';
 import SignUp from './components/SignUp.js';
 
 const RootNavigator = StackNavigator(
   {
+    SignIn: { name: 'SignIn', screen: SignIn },
     SignUp: { name: 'SignUp', screen: SignUp }
   },
   { headerMode: 'screen' }
